e346726891b60a5ddf16262f422a042762caf05b
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2026-02-04 17:01:51 +0530

    feat: implement receiveAttack method and add tests

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -1,3 +1,4 @@
+import { ship } from "../modules/ship";
 
 function gameboard() {
 
@@ -17,20 +18,33 @@ function gameboard() {
     const placeShip = (ship, x, y, direction) => {
         if (direction == 'horizontal') {
             for (let i = 0; i < ship.length; i++) {
-                board[x][y + i] = "X";
+                board[x][y + i] = ship;
             }
         }
 
         if (direction == 'vertical') {
             for (let i = 0; i < ship.length; i++) {
-                board[x + i][y] = "X";
+                board[x + i][y] = ship;
             }
         }
 
     };
 
+    const receiveAttack = (x, y) => {
+        //ship present to mark attack
+        if (board[x][y]) {
+            board[x][y].hit();
+            board[x][y] = "attacked";
+        } else { //ship not present so mark missed
+            board[x][y] = "missed";
+        }
 
-    return { getBoard, placeShip };
+        
+
+    };
+
+
+    return { getBoard, placeShip, receiveAttack };
 }
 
-export {gameboard} ;
+export { gameboard };
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,26 +1,48 @@
 import { gameboard } from "../modules/gameboard";
+import { ship } from "../modules/ship";
 
 test('places a ship horizontally on the board', () => {
     const boardObj = gameboard();
-    const ship = { length: 3 };
+    const myShip = ship(3);
 
-    boardObj.placeShip(ship, 0, 0, 'horizontal');
+    boardObj.placeShip(myShip, 0, 0, 'horizontal');
     const board = boardObj.getBoard();
 
-    expect(board[0][0]).toBe('X');
-    expect(board[0][1]).toBe('X');
-    expect(board[0][2]).toBe('X');
+    expect(board[0][0]).toBe(myShip);
+    expect(board[0][1]).toBe(myShip);
+    expect(board[0][2]).toBe(myShip);
 });
 
 test('places a ship vertically on the board', () => {
     const boardObj = gameboard();
-    const ship = { length: 4 };
+    const myShip = ship(4);
 
-    boardObj.placeShip(ship, 1, 1, 'vertical');
+    boardObj.placeShip(myShip, 1, 1, 'vertical');
     const board = boardObj.getBoard();
 
-    expect(board[1][1]).toBe('X');
-    expect(board[2][1]).toBe('X');
-    expect(board[3][1]).toBe('X');
-    expect(board[4][1]).toBe('X');
+    expect(board[1][1]).toBe(myShip);
+    expect(board[2][1]).toBe(myShip);
+    expect(board[3][1]).toBe(myShip);
+    expect(board[4][1]).toBe(myShip);
+});
+
+test('attacked ship', () => {
+    const boardObj = gameboard();
+    const myShip = ship(3);
+    boardObj.placeShip(myShip, 1, 1, 'horizontal');
+    boardObj.receiveAttack(1, 1);
+    const board = boardObj.getBoard();
+    expect(board[1][1]).toBe('attacked');
+
+});
+
+
+test('missed ship', () => {
+    const boardObj = gameboard();
+    const myShip = ship(3);
+    boardObj.placeShip(myShip, 1, 1, 'horizontal');
+    boardObj.receiveAttack(0, 1);
+    const board = boardObj.getBoard();
+    expect(board[0][1]).toBe('missed');
+
 });
