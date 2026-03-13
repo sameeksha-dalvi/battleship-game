a5f30ae091c86b90b749decba6de5ee7709d947f
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2026-03-13 18:22:55 +0530

    remove pubic path from webpack

--- webpack.common.js
+++ webpack.common.js
@@ -6,7 +6,6 @@ module.exports = {
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist"),
-        publicPath: "/battleship-game/",
         clean: true,
     },
     plugins: [
